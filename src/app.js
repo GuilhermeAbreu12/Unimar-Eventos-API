d427ba28d3f9f8dde96c998fe4c0eed548603eef
GuilhermeAbreu12 <guilhermesabreu08@gmail.com>
2025-09-02 16:39:14 -0300

    feat: add the event query system

--- src/app.js
+++ src/app.js
@@ -3,6 +3,38 @@ const app = express()
 
 const port = 3001
 
+const unimarEvents = [
+    {
+        id: 'CodeXperienceUnimar2017',
+        name: 'Code Xperience Unimar',
+        data: ['24/04/2017', '25/04/2017', '26/04/2017'],
+        link: 'https://www.google.com'
+    },
+    {
+        id: 'UnimarTechSummit2025',
+        name: 'Unimar Tech Summit',
+        data: ['15/04/2025'],
+        link: 'https://www.cursoemvideo.com'
+    },
+    {
+        id: 'Bootcamp2025',
+        name: 'Bootcamp Unimar: Jovem Programador',
+        data: ['25/08/2025'],
+        link: 'https://unimar.br'
+    }
+]
+// Eventos
+let CodeXperienceUnimar;
+
+// Rotas
+
+for (let i = 0; i <= 2; i++)
+    if (unimarEvents[i].name == 'Code Xperience Unimar') CodeXperienceUnimar = unimarEvents[i]
+
+
+app.get('/unimarEvents', (req, res) => res.json(unimarEvents))
+app.get('/unimarEvents/CodeXperienceUnimar', (req, res) => res.json(CodeXperienceUnimar))
+
 // Run
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
